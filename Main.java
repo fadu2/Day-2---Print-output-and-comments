f0c5e7ec8909693cda4818b8c4dec1c4f9816b41
FrancisOhemengA <93487638+fadu2@users.noreply.github.com>
2021-11-02 19:54:14 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,109 @@
+class Main {
+  public static void main(String[] args) {
+    // integer
+    //  boolean
+    // String
+    // double
+    // char
+    // print output
+    // println 
+    // System.out.println("hello world");
+    // // output Regis High School
+    // System.out.println("Regis High School");
+    // System.out.println("Francis Adu");
+
+    // // print  
+    // System.out.print("New ");
+    // System.out.println("York");
+
+    // // CD
+    // // E
+    // System.out.print("C");
+    // System.out.println("D");
+    // System.out.println("E");
+
+    // // F 
+    // // GH 
+    // System.out.println("F");
+    // System.out.print("G");
+    // System.out.println("H");
+
+    // String a = "Regis ";
+    // String b = "High School";
+    // String c = "55 ";
+    // String d = "East 84th Street";
+    // String e = "New ";
+    // String f = "York, ";
+    // String g = "NY ";
+    // String h = "10028";
+
+    // // Regis High School
+    // // 55 East 84th Street
+    // // New York, NY 10028
+
+
+    // System.out.print(a);
+    // System.out.println(b);
+    // System.out.print(c);
+    // System.out.println(d);
+    // System.out.print(e);
+    // System.out.print(f);
+    // System.out.print(g);
+    // System.out.println(h);
+
+    // // Escape characters
+    // // \n -- new line
+    // System.out.println("hello\nworld");
+    // //Francis
+    // //Adu 
+    // System.out.println("Francis\nAdu");
+    // // I love java 
+    // //I 
+    // //love 
+    // //java 
+
+    // System.out.println("I\nlove\njava");
+
+    // // \t -- tab space 
+    // System.out.println("hello\tworld");
+    // System.out.println("Mon\tTued\tWedn");
+
+    // // \ -- backslash
+    // System.out.println("\\");
+    // System.out.println("\\\\");
+
+    // // "
+    // System.out.println(" \" ");
+    // // John said "hi"
+    // System.out.println("John said \"hi\" ");
+
+    // System.out.println("\\n");
+
+    // -- for single line comments 
+    /*  
+      adfsdadsf
+      adfasdfasdfsd
+      adfasdfasfas
+      adfasdfdsadf
+    */ 
+
+      // ;adsfasdfasd
+      // adsfasdfsadfsadf
+
+      /**  
+          javadoc
+      */
+
+      // add + 
+      // substract - 
+      // multiply * 
+      // divide / 
+
+      // 5 times 3 divided 21
+      System.out.println(5 * 3 / 21);
+
+      // 7 divided by 2 times 2
+
+      
+  }
+}
